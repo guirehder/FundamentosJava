f79ef9578ddec216ab07623883e73cf04ff00220
guirehder <guilhermemrehder@gmail.com>
2021-06-13 20:19:28 -0300

    Atualização da atividade calculo de medida

--- src/main/java/fundamentos/Medidas.java
+++ src/main/java/fundamentos/Medidas.java
@@ -14,7 +14,7 @@ public class Medidas {
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
         String opcao;
-        int area = 0; // receber o resultado dos cálculos de áreas
+        double area = 0; // receber o resultado dos cálculos de áreas
 
         System.out.println("Escolha o calculo desejado");
         System.out.println("(1) - Area do Quadrado");
@@ -28,7 +28,13 @@ public class Medidas {
                area = calcularAreaDoQuadrado();
                 break;
             case  "2":
-                // ToDo: calcular área do retangulo
+                area = calcularAreaDoRetangulo();
+                break;
+            case  "3":
+                area = calcularAreaDoTriangulo();
+                break;
+            case  "4":
+                area = calcularAreaDoCirculo();
                 break;
             default:
                 System.out.println("Opção Inválida: " + opcao);
@@ -40,10 +46,37 @@ public class Medidas {
 
     public static int calcularAreaDoQuadrado (){
         int lado; // tamanho do lado do quadrado
-
         System.out.println("Digite o tamanho do lado: ");
         lado = entrada.nextInt(); // leitura do tamanho do lado
         return lado * lado; // retornar a área do quadrado
+    }
+
+    public static int calcularAreaDoRetangulo (){
+        int base; // tamanho da base do retangulo
+        int altura; // tamanho da altura do retangulo
+        System.out.println("Digite o tamanho da base: ");
+        base = entrada.nextInt(); // leitura do tamanho da base
+        System.out.println("Digite o tamanho da altura: ");
+        altura = entrada.nextInt(); // leitura do tamanho da altura
+        return base * altura; // retornar a área do retangulo
+    }
+
+    public static int calcularAreaDoTriangulo (){
+        int base; // tamanho da base do triangulo
+        int altura; // tamanho da altura do triangulo
+        System.out.println("Digite o tamanho da base do triangulo: ");
+        base = entrada.nextInt(); // leitura do tamanho da base
+        System.out.println("Digite o tamanho da altura do triangulo: ");
+        altura = entrada.nextInt(); // leitura do tamanho da altura
+        return ((base * altura)/2) ; // retornar a área do triangulo
+    }
+
+    public static double calcularAreaDoCirculo (){
+        int raio; // tamanho do raio do circulo
+        final double pi = 3.1416; // numero pi
+        System.out.println("Digite tamanho do raio do circulo: ");
+        raio = entrada.nextInt(); // leitura do tamanho da base
+        return ((raio * raio) * pi); // retornar a área do retangulo
     }
 
 }
